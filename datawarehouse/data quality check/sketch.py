41ff7e4eb80356be7c4149928060e01a0c3d1f98
Juandan Luo <1572100494@qq.com>
2024-09-19 19:09:50 -0400

    第一次测试

---
+++ datawarehouse/data quality check/sketch.py
@@ -0,0 +1,6 @@
+import mytests
+tests = {key: value for key, value in mytests.__dict__.items() if key.startswith('test')}
+print(tests.items())
+for testname, test in tests.items():
+    print(test)
+
